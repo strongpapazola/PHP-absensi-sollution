af746f6dd18b00a753e07c9f8f5a09d9e788eef9
strongpapazola <strongpapazola@gmail.com>
2021-08-10 12:43:53 +0700

    init

---
+++ clear-data.php
@@ -0,0 +1,58 @@
+<html>
+<head><title>Contoh Koneksi Mesin Absensi Mengunakan SOAP Web Service</title></head>
+<body bgcolor="#caffcb">
+
+<H3>Clear Log Data</H3>
+
+<?php
+$IP=$_GET["ip"];
+$Key=$_GET["key"];
+if($IP=="") $IP="172.17.1.125";
+if($Key=="") $Key="0";
+?>
+
+<form action="clear-data.php">
+IP Address: <input type="Text" name="ip" value="<?=$IP?>" size=15><BR>
+Comm Key: <input type="Text" name="key" size="5" value="<?=$Key?>"><BR><BR>
+
+<input type="Submit" value="Clear Log">
+</form>
+<BR>
+
+<?php
+if($_GET["ip"]!=""){
+	$Connect = fsockopen($IP, "80", $errno, $errstr, 1);
+	if($Connect){
+		$soap_request="<ClearData><ArgComKey xsi:type=\"xsd:integer\">".$Key."</ArgComKey><Arg><Value xsi:type=\"xsd:integer\">3</Value></Arg></ClearData>";
+		$newLine="\r\n";
+		fputs($Connect, "POST /iWsService HTTP/1.0".$newLine);
+	    fputs($Connect, "Content-Type: text/xml".$newLine);
+	    fputs($Connect, "Content-Length: ".strlen($soap_request).$newLine.$newLine);
+	    fputs($Connect, $soap_request.$newLine);
+		$buffer="";
+		while($Response=fgets($Connect, 1024)){
+			$buffer=$buffer.$Response;
+		}
+	}else echo "Koneksi Gagal";
+
+	function Parse_Data($data,$p1,$p2){
+		$data=" ".$data;
+		$hasil="";
+		$awal=strpos($data,$p1);
+		if($awal!=""){
+			$akhir=strpos(strstr($data,$p1),$p2);
+			if($akhir!=""){
+				$hasil=substr($data,$awal+strlen($p1),$akhir-strlen($p1));
+			}
+		}
+		return $hasil;	
+	}
+	$buffer=Parse_Data($buffer,"<Information>","</Information>");
+	echo "<B>Result:</B><BR>";
+	echo $buffer;
+}	
+?>
+
+</body>
+</html>
+
